afef110e7aaec7897857a06dbc4ba00d685e9d10
salamafazlul <105430673+salamafazlul@users.noreply.github.com>
2025-09-19 23:05:23 +0530

    Initial commit from Create Next App

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold text-rose-500">Home</h1>
+      <Button>Click me</Button>
+    </div>
+  );
+}
