9a4f2a49ffe10f5682f346048afedc93016568e8
salamafazlul <105430673+salamafazlul@users.noreply.github.com>
2025-09-23 02:08:46 +0530

    Update dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,14 +1,22 @@
 import EmptyState from "@/components/ui/empty-state";
+import {
+  deleteProjectById,
+  duplicateProjectById,
+  editProjectById,
+  getAllPlaygroundForUser,
+} from "@/features/dashboard/actions";
 import AddNewButton from "@/features/dashboard/components/add-new-button";
-import AddRepo from "@/features/dashboard/components/app-repo-button";
+import AddRepoButton from "@/features/dashboard/components/app-repo-button";
+import ProjectTable from "@/features/dashboard/components/project-table";
 
 const Page = async () => {
-  const playgrounds: any[] = [];
+  const playgrounds = await getAllPlaygroundForUser();
+
   return (
     <div className="flex flex-col justify-start items-center min-h-screen mx-auto max-w-7xl px-4 py-10">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6 w-full">
         <AddNewButton />
-        <AddRepo />
+        <AddRepoButton />
       </div>
       <div className="mt-10 flex flex-col justify-center items-center w-full">
         {playgrounds && playgrounds.length === 0 ? (
@@ -18,8 +26,13 @@ const Page = async () => {
             imageSrc="/empty-state.svg"
           />
         ) : (
-          // todo: add playground table
-          <p>Playground Table</p>
+          <ProjectTable
+            // @ts-ignore
+            projects={playgrounds || []}
+            onDeleteProject={deleteProjectById}
+            onUpdateProject={editProjectById}
+            onDuplicateProject={duplicateProjectById}
+          />
         )}
       </div>
     </div>
